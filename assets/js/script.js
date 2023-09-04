06805c1fa882a2c8c1f87e48181f32fcc1c94381
“LiciaLicari” <licianbsplicari@gmail.com>
2023-09-04 13:59:40 +0200

    add match selection logic
    need fixing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,12 @@
 console.log("miao");
 
+//variabili per le opzioni di gioco
+const selectMatchEl = document.getElementById('difficulty_level_select');
+const easyMatchEl = document.getElementById('easy');
+const midMatchEl = document.getElementById('mid');
+const hardMatchEl = document.getElementById('hard');
+console.log(selectMatchEl, easyMatchEl, midMatchEl, hardMatchEl);
+
 /*
 Consegna
 - L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
@@ -7,40 +14,107 @@ Consegna
 
 
 // - Ogni cella ha un numero progressivo, da 1 a 100 (Ci saranno quindi 10 caselle per ognuna delle 10 righe)
-const bodyEl = document.querySelector("body")
+const bodyEl = document.querySelector("body");
 const fieldEl = document.querySelector(".field");
-const limit = 100 // numero delle celle
+const limit = 100; // numero delle celle
+const midLimit = 81;
+const hardLimit = 49;
 
 //richiamo il pulsante
 const clickButton = document.querySelector('.spawn');
 
-//addeventlistener per farlo funzionare
+//condizione a seconda del livello scelto
 
-clickButton.addEventListener('click', function () {
-    clickButton.classList.toggle('d-none');
+if (easyMatchEl) {
+
     
-    spawnField(fieldEl, limit);
-    function spawnField(domEl, limit) {
-        //creo il campo
-
-        for (let i = 0; i < limit; i++) {
-            const cellEl = document.createElement("div");
-            cellEl.classList = "cell";
-            cellEl.innerHTML = i + 1;
-            fieldEl.append(cellEl);
-
-            //- emetto un messaggio in console con il numero della cella cliccata.
-            cellEl.addEventListener("click", function () {
-                console.log("Click on cell: ", cellEl);
-                // - Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro 
-                cellEl.classList.add("bg-sky");
-            })
-
-
+    clickButton.addEventListener('click', function () {
+        clickButton.classList.toggle('d-none');
+        
+        spawnField(fieldEl, limit);
+        function spawnField(domEl, limit) {
+            //creo il campo
+    
+            for (let i = 0; i < limit; i++) {
+                const cellEl = document.createElement("div");
+                cellEl.classList = "cell";
+                cellEl.innerHTML = i + 1;
+                fieldEl.append(cellEl);
+    
+                //- emetto un messaggio in console con il numero della cella cliccata.
+                cellEl.addEventListener("click", function () {
+                    console.log("Click on cell: ", cellEl);
+                    // - Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro 
+                    cellEl.classList.add("bg-sky");
+                })
+    
+    
+            }
+    
         }
+    })
 
-    }
-})
+} else if (midMatchEl) {
+
+    clickButton.addEventListener('click', function () {
+        clickButton.classList.toggle('d-none');
+        
+        spawnField(fieldEl, midLimit);
+        function spawnField(domEl, midLimit) {
+            //creo il campo
+    
+            for (let i = 0; i < midLimit; i++) {
+                const cellMid_El = document.createElement("div");
+                cellMid_El.classList = "cell_mid";
+                cellMid_El.innerHTML = i + 1;
+                fieldEl.append(cellMid_El);
+    
+                //- emetto un messaggio in console con il numero della cella cliccata.
+                cellMid_El.addEventListener("click", function () {
+                    console.log("Click on cell: ", cellMid_El);
+                    // - Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro 
+                    cellMid_El.classList.add("bg-sky");
+                })
+    
+    
+            }
+    
+        }
+    })
+
+} else { //hardMAtch
+
+    clickButton.addEventListener('click', function () {
+        clickButton.classList.toggle('d-none');
+        
+        spawnField(fieldEl, hardLimit);
+        function spawnField(domEl, hardLimit) {
+            //creo il campo
+    
+            for (let i = 0; i < hardLimit; i++) {
+                const cellHard_El = document.createElement("div");
+                cellHard_El.classList = "cell_hard";
+                cellHard_El.innerHTML = i + 1;
+                fieldEl.append(cellHard_El);
+    
+                //- emetto un messaggio in console con il numero della cella cliccata.
+                cellHard_El.addEventListener("click", function () {
+                    console.log("Click on cell: ", cellHard_El);
+                    // - Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro 
+                    cellHard_El.classList.add("bg-sky");
+                })
+    
+    
+            }
+    
+        }
+    })
+}
+
+
+   
+
+
 
 /* 
 Consegna
